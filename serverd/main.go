fa514a52945a072e204b03fe731b0e2affc9e9dd
muhdjabir <e0725438@u.nus.edu>
2023-09-02 21:49:25 +0800

    Initialize docker files and web service

--- serverd/main.go
+++ serverd/main.go
@@ -3,9 +3,25 @@ package main
 import (
 	"GolangAPIAssessment/database"
 	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	log.Printf("This is my Golang log")
 	database.Connect()
+	setRoutes()
+}
+
+func setRoutes() {
+	router := gin.Default()
+	log.Println("Server is listening at PORT:8080")
+
+	public := router.Group("/api")
+	public.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Welcome to OneCV"})
+	})
+
+	router.Run("0.0.0.0:8080")
 }
